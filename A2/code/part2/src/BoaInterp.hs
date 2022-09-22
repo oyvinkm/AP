dacd4ccfec3a0fc2dfae614d2b36687d11ec29c1
AskePetersen <aske98@live.com>
2022-09-22 17:55:21 +0200

    missing report and Compr

--- A2/code/part2/src/BoaInterp.hs
+++ A2/code/part2/src/BoaInterp.hs
@@ -15,6 +15,51 @@ type Env = [(VName, Value)]
 data RunError = EBadVar VName | EBadFun FName | EBadArg String
   deriving (Eq, Show)
 
+-- Check if length of list is in range [1,3]
+checkLen :: [Value] -> Bool
+checkLen vs = case operate Less  (IntVal (length vs)) (IntVal 4) of
+                Right TrueVal -> case vs of
+                                  [] -> False
+                                  _ -> True
+                Right FalseVal -> False
+                Right _ -> False
+                Left _ -> False
+
+-- Checks if all elements in list are type IntVal, should not be called with empty list.
+isIntList :: [Value] -> Bool
+isIntList (x:xs) = case x of
+                        IntVal _ -> isIntList xs
+                        _ -> False
+isIntList [] = True
+
+listToString :: [Value] -> String
+listToString [] = ""
+listToString (x:xs) = let res = case x of
+                                  NoneVal -> "None"
+                                  IntVal a -> show a
+                                  TrueVal -> show True
+                                  FalseVal -> show False 
+                                  StringVal s -> s 
+                                  ListVal (x:xs) -> "[" ++ listToString [x] ++ case xs of 
+                                                                                  [] -> listToString xs ++ "]"
+                                                                                  _ -> ", " ++ listToString xs ++ "]"
+                                  ListVal [] -> "[]"
+                      in res ++ case xs of
+                                   [] -> "" 
+                                   _ -> ", " ++ listToString xs
+
+concatStrings :: [Value] -> String
+concatStrings [] = "" 
+concatStrings (v:vs) = let res = case v of
+                                    NoneVal -> "None"
+                                    IntVal a -> show a
+                                    TrueVal -> show True
+                                    FalseVal -> show False 
+                                    StringVal s -> s 
+                                    ListVal xs -> "[" ++ listToString xs ++ "]"
+                        in res ++ case vs of
+                                   [] -> "" ++ concatStrings vs
+                                   _ -> " " ++ concatStrings vs
 newtype Comp a = Comp {runComp :: Env -> (Either RunError a, [String]) }
 
 instance Monad Comp where
@@ -23,11 +68,6 @@ instance Monad Comp where
                             (Left e, s) -> (Left e, s)
                             (Right a, s) -> let (x1, s1) = runComp (f a) r
                                             in (x1, s <> s1))
-
-
-  -- m >>= f = Comp (\r -> do (Right b, s1) <- runComp m r
-  --                          (Right c, s2) <- runComp (f (Right b)) r
-  --                          return (c, s1 <> s2))
 
 -- You shouldn't need to modify these r : (x, v)
 instance Functor Comp where
@@ -64,10 +104,12 @@ operate :: Op -> Value -> Value -> Either String Value
 operate Plus (IntVal v1) (IntVal v2) = Right (IntVal (v1 + v2))
 operate Minus (IntVal v1) (IntVal v2) = Right (IntVal (v1 - v2))
 operate Times (IntVal v1) (IntVal v2) = Right (IntVal (v1 * v2))
-operate Div (IntVal v1) (IntVal v2) = Right (IntVal (v1 `div` v2))
+operate Div (IntVal v1) (IntVal v2) = case v2 of
+                                        0 -> error "divide by zero"
+                                        _ -> Right (IntVal (v1 `div` v2))
 operate Mod (IntVal v1) (IntVal v2) = Right (IntVal (v1 `mod` v2))
-operate Eq v1 v2 = if v1 == v2 then (Right TrueVal)
-                      else (Right FalseVal)
+operate Eq v1 v2 = if v1 == v2 then Right TrueVal
+                      else Right FalseVal
 operate Less  (IntVal v1) (IntVal v2) = if v1 < v2 then Right TrueVal
                       else Right FalseVal
 operate Greater (IntVal v1) (IntVal v2) = if v1 > v2 then Right TrueVal
@@ -77,78 +119,53 @@ operate In v1 (ListVal v2) = if v1 `elem` v2 then Right TrueVal
 
 operate _ _ _ = Left "Error, the operator couldn't handle the arguments."
 
--- Check if length of list is in range [1,3]
-checkLen :: [Value] -> Bool
-checkLen vs = case operate Less  (IntVal (length vs)) (IntVal 4) of
-                Right TrueVal -> case vs of
-                                  [] -> False
-                                  _ -> True
-                Right FalseVal -> False
-                Right _ -> False
-                Left _ -> False
-
--- Checks if all elements in list are type IntVal, should not be called with empty list.
-isIntList :: [Value] -> Bool
-isIntList (x:xs) = case x of
-                        IntVal _ -> True && isIntList xs
-                        _ -> False
-isIntList [] = True
-
-listToString :: [Value] -> String
-listToString [] = ""
-listToString (x:xs) = let res = case x of
-                                  NoneVal -> "None"
-                                  IntVal a -> show a
-                                  TrueVal -> show True
-                                  FalseVal -> show False 
-                                  StringVal s -> s 
-                                  ListVal (x:xs) -> "[" ++ listToString [x] ++ case xs of 
-                                                                                  [] -> listToString xs ++ "]"
-                                                                                  _ -> ", " ++ listToString xs ++ "]"
-                                  ListVal [] -> "[]"
-                      in res ++ case xs of
-                                   [] -> "" 
-                                   _ -> ", " ++ listToString xs
-
-concatStrings :: [Value] -> String
-concatStrings [] = "" 
-concatStrings (v:vs) = let res = case v of
-                                    NoneVal -> "None"
-                                    IntVal a -> show a
-                                    TrueVal -> show True
-                                    FalseVal -> show False 
-                                    StringVal s -> s 
-                                    ListVal xs -> "[" ++ listToString xs ++ "]"
-                        in res ++ case vs of
-                                   [] -> "" ++ concatStrings vs
-                                   _ -> " " ++ concatStrings vs
 
 apply :: FName -> [Value] -> Comp Value
-apply "range" vs = if (checkLen vs && isIntList vs) then 
+apply "range" vs = if checkLen vs && isIntList vs then 
                     case vs of
                       [IntVal a] -> return (ListVal (map IntVal [0 .. a-1]))
                       [IntVal a, IntVal b] ->  return (ListVal (map IntVal [a .. b-1]))
-                      [IntVal a, IntVal b, IntVal c] -> if ((a > b && c>0) || (a < b && c < 0)) then return(ListVal [])
+                      [IntVal a, IntVal b, IntVal c] -> if (a > b && c>0) || (a < b && c < 0) then return(ListVal [])
                                                         else return (ListVal (map IntVal [a, a+c .. b - (c `div` abs c)]))
                       _ -> undefined
                     else abort (EBadArg "Argument is invalid.")
-apply "print" vs = do _ <- output $ concatStrings vs; return (NoneVal)
+apply "print" vs = do _ <- output $ concatStrings vs; return NoneVal
 apply f _ = abort (EBadFun f)
-
 
 -- Main functions of interpreter
 eval :: Exp -> Comp Value
-eval = undefined
+eval (Const v) = return v
+eval (Var x) = look x
+eval (Oper op e1 e2) = do x <- eval e1
+                          y <- eval e2
+                          case operate op x y of
+                            Right a -> return a
+                            Left e -> abort $ EBadArg e
+eval (Not e) = do a <- eval e
+                  if truthy a then return FalseVal 
+                    else return TrueVal
+eval (Call f es) = do vals <- mapM eval es
+                      apply f vals
+eval (List es) = do xs <- mapM eval es
+                    return (ListVal xs)
+
 
 exec :: Program -> Comp ()
-exec = undefined
+exec [] = return ()
+exec (y:ys) = case y of
+                SDef v e -> do res <- eval e
+                               withBinding v res (exec ys)
+                SExp e -> do _ <- eval e 
+                             exec ys
+
 
 execute :: Program -> ([String], Maybe RunError)
-execute = undefined
+execute p = let (eev, strs) = runComp (exec p) []
+            in case eev of 
+              Right () -> (strs, Nothing)
+              Left err -> (strs, Just err)
 
 
-
--- ListOfFalse = [None, False, 0, "", []]
 
 ---------TESTS
 -- truthy (ListVal [])
@@ -159,3 +176,12 @@ execute = undefined
 -- concatStrings [ListVal []]
 -- [[1,2],None,[]]
 -- concatStrings [ListVal[ListVal[IntVal 1, IntVal2], NoneVal, ListVal[]]]
+
+
+-- ccHelper :: CClause -> Exp -> Comp [Value]
+-- ccHelper (CCFor v ec) e = do
+--                         l <- eval ec
+--                         case l of
+--                           ListVal (x:xs) -> return [withBinding v x (eval e) | x <- xs]
+--                           ListVal [] -> return (ListVal [])
+--                           _ ->  abort (EBadArg "Argument is invalid.")
